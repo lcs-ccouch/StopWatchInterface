20bedf1e97f7339f4ef39ef538e4efa9f1e3015c
Calum Couch <ccouch@lcs.on.ca>
2022-11-15 14:15:54 -0500

    Initial Commit

---
+++ StopWatchInterface/StopWatchInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchInterfaceApp.swift
+//  StopWatchInterface
+//
+//  Created by calum couch on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
